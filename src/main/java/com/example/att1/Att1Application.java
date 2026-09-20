d1c6a9d3896c88817f4a92c85def9923e5c8b71f
staleray <148722557+staleray@users.noreply.github.com>
2026-09-20 21:19:56 +0200

    Add src/main/java/com/example/att1

---
+++ src/main/java/com/example/att1/Att1Application.java
@@ -0,0 +1,13 @@
+package com.example.att1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Att1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Att1Application.class, args);
+	}
+
+}
